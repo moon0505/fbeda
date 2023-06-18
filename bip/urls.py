4bef2b8fbee25ad5b549ec12ec8c2747c1c02eed
moon0505 <krsgnzlz@gmail.com>
2023-06-17 21:30:14 -0700

    First Commit

---
+++ bip/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
+
+
+
+
+app_name = 'bip'
+
+urlpatterns = [
+    # path('home/', views.home, name='home'),
+]
