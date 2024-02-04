1657b91c38c6c64801766a9dce1b2ab4fa0d5702
Kristian <krsgnzlz@gmail.com>
2024-02-04 15:23:21 -0800

    deploy to railway

--- functional_behavior_analysis/settings.py
+++ functional_behavior_analysis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rhv&4!$5^hjj=zfv%@$j&0gym_*e64u1uz6*s0m@a10aij%r5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
