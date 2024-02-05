3322782534b3ae26d9e00e0bc0f6df046d9b689c
Kristian <krsgnzlz@gmail.com>
2024-02-04 19:54:25 -0800

    add postgress to railway

--- functional_behavior_analysis/settings.py
+++ functional_behavior_analysis/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_URL = "postgresql://postgres:6a3Da-FbEc2beFCgBdb-acCec5e-FfBB@viaduct.proxy.rlwy.net:21269/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +30,6 @@ SECRET_KEY = 'django-insecure-rhv&4!$5^hjj=zfv%@$j&0gym_*e64u1uz6*s0m@a10aij%r5z
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -86,15 +88,23 @@ WSGI_APPLICATION = 'functional_behavior_analysis.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'functionaldb',
-        'USER': 'postgres',
-        'PASSWORD': 'Luna0505@',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    
+  
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'functionaldb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Luna0505@',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
