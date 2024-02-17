66842357c7181f0c42cabd66dcc6964e2519db57
Kristian <krsgnzlz@gmail.com>
2024-02-17 06:36:04 -0800

    update mediaa

--- functional_behavior_analysis/urls.py
+++ functional_behavior_analysis/urls.py
@@ -20,9 +20,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from bip import views
-
-
-
+from django.views.static import serve 
 
 
 
@@ -35,9 +33,6 @@ urlpatterns = [
 
   
 ]
-
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
