a27879e15ebc20566a01c96d5b03f3c454ca129e
Kristian <krsgnzlz@gmail.com>
2024-02-25 09:02:26 -0800

    added customuser to signal

--- bip/signals.py
+++ bip/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.contrib.auth.models import CustomUser
+from bip.models import CustomUser
 
 @receiver(post_save, sender=CustomUser)
 def notify_superusers_on_new_user(sender, instance, created, **kwargs):
