54b81efcc6196528d740a65753a9a20fb401e443
Kristian <krsgnzlz@gmail.com>
2024-02-25 08:39:21 -0800

    email signel

--- bip/apps.py
+++ bip/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BipConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bip'
+
+
+    def ready(self):
+        # Make sure to replace 'bip.signals' with the correct path to your signals module.
+        # This import statement ensures your signals are connected at startup.
+        import bip.signals
--- functional_behavior_analysis/settings.py
+++ functional_behavior_analysis/settings.py
@@ -168,9 +168,13 @@ LOGIN_REDIRECT_URL='/afterlogin'
 
 
 #SMTP Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'exploratoryfba@gmail.com'
 EMAIL_HOST_PASSWORD = 'iadb tmfk nwti swry'
+
+
