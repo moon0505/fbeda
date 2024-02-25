f251bbb94bdd9fe63a80ff04fea0b59771aff90a
Kristian <krsgnzlz@gmail.com>
2024-02-25 11:50:57 -0800

    account status send email

--- bip/signals.py
+++ bip/signals.py
@@ -1,9 +1,14 @@
+from django.db.models.signals import pre_save
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 
 from .models import CustomUser
+from .models import CaseManager
+
+
+
 
 
 @receiver(post_save, sender=CustomUser)
@@ -14,3 +19,20 @@ def notify_superusers_on_new_user(sender, instance, created, **kwargs):
         message = f'A new user has registered: {instance.username} (email: {instance.email})'
         from_email = 'noreply@exploratoryfba.com'  # Use an appropriate sender email
         send_mail(subject, message, from_email, superusers_emails, fail_silently=False)
+
+
+
+@receiver(pre_save, sender=CaseManager)
+def send_status_active_email(sender, instance, **kwargs):
+    # Attempt to fetch the instance from the database to compare statuses
+    if instance.pk:
+        old_instance = CaseManager.objects.filter(pk=instance.pk).first()
+        if old_instance and not old_instance.status and instance.status:
+            # The status changed from False to True
+            send_mail(
+                'Your Account is Active',
+                f'Hello, your account is active',
+                'exploratoryfba.com',
+                [instance.user.email],
+                fail_silently=False,
+            )
